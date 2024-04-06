276a7ac708421b93df07ea227001d247ce262bbf
Iegor Azuaga <iegor.azuaga@mayoral.com>
2024-04-06 17:24:39 +0200

    chore: missing lines for repro

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,13 @@
+import { headers } from 'next/headers';
 
 export default async function NotFound() {
+  const headerList = headers();
+  const acceptLanguage = headerList.get('accept-language');
+
   return (
     <div>
       <p>404 - Page Not Found</p>
+      <p>accept-language: {acceptLanguage}</p>
     </div>
   );
 }
